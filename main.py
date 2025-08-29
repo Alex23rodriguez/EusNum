b161f15bce829b89ba1c431abaa47ce748a50418
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2025-08-29 20:16:03 +0200

    feat: added main game logic

--- main.py
+++ main.py
@@ -1,6 +1,82 @@
-def main():
-    print("Hello from eusnum!")
+import json
+import os
+from random import choice, random
+
+from cligame import Game
+
+from util import concat_audio_files, number_to_word
+
+# %%
+with open("data/unique_words.json", "r") as f:
+    unique_words = json.load(f)
+
+nums = {v: k for k, v in unique_words.items()}
+
+audio_dir = "./audio"
 
 
-if __name__ == "__main__":
-    main()
+# %%
+numbers = list(range(60)) + [60, 80, 100] * 5
+
+
+# %%
+def rev_map(sentence):
+    words = sentence.split(" ")
+    return [nums[w] for w in words]
+
+
+# %%
+# Game modes
+
+
+def audio_quiz(_):
+    n = choice(numbers)
+    word = number_to_word(n)
+
+    concat_audio_files(
+        rev_map(word), audio_dir, output_file="tmp/result.wav", ext="wav"
+    )
+    os.system("afplay tmp/result.wav")
+
+    ans = input(f"Which number did you hear? ")
+    correct_ans = str(n)
+
+    return ans == correct_ans, f"{correct_ans}: {word}"
+
+
+def spelling_quiz(_):
+    n = choice(numbers)
+    word = number_to_word(n)
+
+    ans = input(f"Spell the number {n}: ")
+    correct_ans = word
+
+    return ans == correct_ans, f"{correct_ans}"
+
+
+def mixed_quiz(_):
+    if random() < 0.5:
+        return audio_quiz(_)
+    else:
+        return spelling_quiz(_)
+
+
+# %%
+
+
+# choose game
+prompt = """Available games:
+    1 - spelling
+    2 - audio
+    3 - mixed
+Game: """
+
+games = {
+    "1": spelling_quiz,
+    "2": audio_quiz,
+    "3": mixed_quiz,
+}
+
+mygame = Game(games[input(prompt)])
+mygame.start()
+mygame.save_raw("stats/stats.json")
